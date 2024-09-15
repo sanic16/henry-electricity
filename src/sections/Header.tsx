b2cec5a8c0d43dd2839fa0ab078dbe668d296676
Julio Rubén Sanic Martínez <julio.sanic.gt.256@gmail.com>
2024-09-15 14:19:05 -0600

    header accent

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className="w-full h-full relative">
         <ImageTheme images={images} />
         <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full max-w-md p-4 flex flex-col gap-4 items-center md:max-w-3xl xl:gap-8 xl:max-w-4xl">
-          <h1 className="font-semibold text-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl uppercase bg-clip-text text-transparent bg-gradient-to-r from-blue-200 to-white dark:from-yellow-200 dark:to-white ">
+          <h1 className="font-semibold text-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl uppercase bg-clip-text text-transparent bg-gradient-to-r from-blue-200 to-white dark:from-yellow-200 dark:to-white pt-2 lg:pt-4">
             Soluciones Eléctricas
           </h1>
           <p className="text-xl text-center max-w-lg text-blue-200 dark:text-yellow-100 ">
